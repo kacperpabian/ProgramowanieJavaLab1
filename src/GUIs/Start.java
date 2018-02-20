0630577ab86b9aa3bc267dcb12e66eae63f38562
kacper <226123@student.pwr.edu.pl>
2018-02-20 08:25:04 +0100

    pierwszy

---
+++ src/GUIs/Start.java
@@ -0,0 +1,9 @@
+package GUIs;
+
+public class Start {
+    public void test()
+    {
+        System.out.println("321");
+    }
+
+}
---
+++ src/MainApp.java
@@ -0,0 +1,9 @@
+import GUIs.Start;
+
+public class MainApp {
+    public static void main(String arg[])
+    {
+        Start start = new Start();
+        start.test();
+    }
+}
